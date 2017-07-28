19e60fb1e9740a387f0f9f6df4f00bda60ac281a
Ash <jjs2ut@gmail.com>
2017-07-28 11:05:43 +0900

    main

--- keras_test/main.py
+++ keras_test/main.py
@@ -10,13 +10,8 @@ import keras.callbacks
 import keras.backend.tensorflow_backend as KTF
 import tensorflow as tf
 from tensorboard_environment import TensorboardEnvironment
-
-def load_mnist(npy_dir=os.path.join('C:\\', 'Users', 'ash', 'Downloads', 'mnist_npy')):
-    x_train = np.load(os.path.join(npy_dir, 'x_train.npy'))
-    x_test = np.load(os.path.join(npy_dir, 'x_test.npy'))
-    y_train = np.load(os.path.join(npy_dir, 'y_train.npy'))
-    y_test = np.load(os.path.join(npy_dir, 'y_test.npy'))
-    return (x_train, y_train), (x_test, y_test)
+from load_mnist import load_mnist
+from my_model import simple_full_connected, vgg_like
 
 os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
 os.environ["CUDA_VISIBLE_DEVICES"]="-1"
@@ -26,46 +21,17 @@ n_classes = 10
 epochs   = 5
 n_data    = 28*28
 log_filepath = '../log'
-npy_dir = os.path.join('C:\\', 'Users', 'ash', 'Downloads', 'mnist_npy')
 
 # load data
-print("Loading the data...")
-(x_train, y_train), (x_test, y_test) = load_mnist(npy_dir=os.path.join('C:\\', 'Users', 'ash', 'Downloads', 'mnist_npy'))
-print("Completed")
-
-# reshape
-x_train = x_train.reshape(x_train.shape[0], x_train.shape[1]*x_train.shape[2])
-x_test = x_test.reshape(x_test.shape[0], x_test.shape[1]*x_test.shape[2])
-
-# rescale
-x_train = x_train.astype(np.float32)
-x_test  = x_test.astype(np.float32)
-x_train /= 255
-x_test  /= 255
-
-# convert class vectors to binary class matrices (one hot vectors)
-y_train = np_utils.to_categorical(y_train, n_classes)
-y_test = np_utils.to_categorical(y_test, n_classes)
+(x_train, y_train), (x_test, y_test) = load_mnist()
 
 with TensorboardEnvironment():
-    model = Sequential()
-    model.add(Dense(64, input_shape=(n_data,), init='normal', name='dense1'))
-    model.add(Activation('relu', name='relu1'))
-    model.add(Dropout(0.2, name='dropout1'))
-    model.add(Dense(64, init='normal', name='dense2'))
-    model.add(Activation('relu', name='relu2'))
-    model.add(Dropout(0.2, name='dropout2'))
-    model.add(Dense(10, init='normal', name='dense3'))
-    model.add(Activation('softmax', name='softmax1'))       
+    model = simple_full_connected()
     model.summary()
-
     model.compile(loss='categorical_crossentropy', optimizer=SGD(lr=0.01), metrics=['accuracy'])
 
-    tb_cb = keras.callbacks.TensorBoard(log_dir=log_filepath, histogram_freq=1)
-    cbks = [tb_cb]
-
-    history = model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, verbose=1, callbacks=cbks, validation_data=(x_test, y_test))
-
+    tensorboard_callback = keras.callbacks.TensorBoard(log_dir=log_filepath, histogram_freq=1)
+    history = model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, verbose=1, callbacks=[tensorboard_callback], validation_split=0.1)
     score = model.evaluate(x_test, y_test, verbose=0)
     print('Test score:', score[0])
     print('Test accuracy;', score[1])
